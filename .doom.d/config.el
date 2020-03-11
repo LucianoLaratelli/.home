76407b61af192c1fe712b48446a25e7294197fe8
Luciano Laratelli <luciano@laratelli.com>
2020-03-11 19:41:54 -0400

    remove unused aliases; format; update update

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -97,3 +97,19 @@
 
 (map! :leader
       "r" #'rtags-find-symbol-at-point)
+
+(defun eval-region-or-buffer ()
+  (interactive)
+  (let ((debug-on-error t))
+    (cond
+     (mark-active
+      (call-interactively 'eval-region)
+      (message "Region evaluated!")
+      (setq deactivate-mark t))
+     (t
+      (eval-buffer)
+      (message "Buffer evaluated!")))))
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-x E") 'eval-region-or-buffer)))
