30260e4896cd5201ca9b9c4ae44e8cee82c60c18
Luciano Laratelli <luciano@laratelli.com>
2020-03-11 11:26:51 -0400

    POSframe, not POSTframe

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -22,7 +22,7 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 (package! bison-mode)
-(package! which-key-postframe)
+(package! which-key-posframe)
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
