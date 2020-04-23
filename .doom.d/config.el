80940a4d2670d4deb3d8d2c66ff766a0d6405a05
Luciano Laratelli <luciano@laratelli.com>
2020-04-23 10:19:57 -0400

    add my/org-begin-template

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -134,7 +134,7 @@
 (use-package! make-mode
   :defer t)
 
-(use-package flycheck-clang-tidy
+(use-package! flycheck-clang-tidy
   :after flycheck
   :hook
   (flycheck-mode . flycheck-clang-tidy-setup)
@@ -183,6 +183,50 @@
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; function to wrap blocks of text in org templates                       ;;
+;; e.g. latex or src etc                                                  ;;
+;; http://pragmaticemacs.com/emacs/wrap-text-in-an-org-mode-block/        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun my/org-begin-template ()
+  "Make a template at point."
+  (interactive)
+  (if (org-at-table-p)
+      (call-interactively 'org-table-rotate-recalc-marks)
+    (let* ((choices '(("s" . "SRC")
+                      ("e" . "EXAMPLE")
+                      ("q" . "QUOTE")
+                      ("v" . "VERSE")
+                      ("c" . "CENTER")
+                      ("l" . "LaTeX")
+                      ("h" . "HTML")
+                      ("a" . "ASCII")))
+           (key
+            (key-description
+             (vector
+              (read-key
+               (concat (propertize "Template type: " 'face 'minibuffer-prompt)
+                       (mapconcat (lambda (choice)
+                                    (concat (propertize (car choice) 'face 'font-lock-type-face)
+                                            ": "
+                                            (cdr choice)))
+                                  choices
+                                  ", ")))))))
+      (let ((result (assoc key choices)))
+        (when result
+          (let ((choice (cdr result)))
+            (cond
+             ((region-active-p)
+              (let ((start (region-beginning))
+                    (end (region-end)))
+                (goto-char end)
+                (insert "#+END_" choice "\n")
+                (goto-char start)
+                (insert "#+BEGIN_" choice "\n")))
+             (t
+              (insert "#+BEGIN_" choice "\n")
+              (save-excursion (insert "#+END_" choice))))))))))
 
 (setq backup-directory-alist `(("." . "~/.BACKUPS")))
 (setq backup-by-copying t)
