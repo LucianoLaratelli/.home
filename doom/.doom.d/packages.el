332f492a65d07359dc684350aa4b9d23f5abc5a1
Luciano Laratelli <luciano@laratelli.com>
2020-05-03 12:13:05 -0400

    remove symon

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -34,11 +34,6 @@
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
 
-(package! symon :recipe
-         (:host github
-          :repo "ieure/symon"
-          :files ("*.el")))
-
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
