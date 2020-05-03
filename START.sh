e4df4c42a3e645350ed09f1ed42abad82b886e9b
Luciano Laratelli <luciano@laratelli.com>
2020-05-03 12:09:38 -0400

    update start to clone and compile emacs

--- START.sh
+++ START.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-pushd ~ || (
-	echo "pushd failed to push ~ onto directory stack"
-	exit
-)
-pushd ~/.home || (
-	echo "pushd failed to push ~/.home onto directory stack"
-	exit
-)
+cd ~/.home
 stow bash doom git tmux vim
-popd || (
-	echo "popd failed to pop top of directory stack"
-	exit
-)
+source ~/.bashrc
+
+mkdir ~/repos
+cd ~/repos
+
+git clone git@github.com:rupa/z.git
+
+git clone git@github.com:emacs-mirror/emacs.git
+cd emacs
+git checkout -t origin/emacs-26
+./autogen.sh
+CFLAGS="-O2" ./configure --with-modules --quiet
+NUMCORES="$(grep -c ^processor /proc/cpuinfo)"
+NUMCORES=$(($NUMCORES * 2))
+make -j${NUMCORES}
+
+cd
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 yes | ~/.emacs.d/bin/doom install
 doom compile
-pushd ~/repos || (
-	echo "pushd failed to push ~/repos onto directory stack"
-	exit
-)
-git clone git@github.com:rupa/z.git
-popd
