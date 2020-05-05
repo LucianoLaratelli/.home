a53c7b9f9888af2145b392ba124ded222ca22514
Luciano E. Laratelli <luciano@laratelli.com>
2020-05-05 14:21:05 -0400

    add ude; change org dir; remove unecessary pushds

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -32,7 +32,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dropbox/org/")
+(setq org-roam-directory "~/Dropbox/org_roam")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
