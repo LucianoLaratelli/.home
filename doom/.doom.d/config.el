89048ebb5af708a0913a55f076081b75a96d3323
Luciano E. Laratelli <luciano@laratelli.com>
2020-05-07 16:43:30 -0400

    Remove explicit org-roam dir

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -33,7 +33,6 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Dropbox/org/")
-(setq org-roam-directory "~/Dropbox/org_roam")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
