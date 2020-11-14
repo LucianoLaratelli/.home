9a51a208af46b68aaa7452aa0eeb51d5c1ad0c96
Luciano Laratelli <luciano@laratelli.com>
2020-11-13 23:53:28 -0500

    sort packages

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -22,12 +22,14 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 (package! bison-mode)
+(package! bnf-mode)
 (package! ccls)
-(package! impatient-mode)
-(package! open-junk-file)
-(package! make-mode)
 (package! flycheck-clang-tidy)
+(package! impatient-mode)
 (package! magit-delta)
+(package! make-mode)
+(package! ob-prolog)
+(package! open-junk-file)
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
