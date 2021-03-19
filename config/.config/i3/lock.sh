aad55ee5c1a36a42fb187e917d1fb709cefc1e85
Luciano Laratelli <luciano@Lucianos-MacBook-Pro.local>
2021-03-19 18:02:09 -0400

    Delete linux-only files

--- config/.config/i3/lock.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#
-# Locks the screen immediately
-i3lock -c 239969
-# Kills last background task so idle timer doesn't keep running
--- config/.config/polybar/launch.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env sh
-
-killall -q polybar
-
-polybar --reload mybar &
